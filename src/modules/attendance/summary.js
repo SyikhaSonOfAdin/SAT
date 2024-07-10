3a9e3f712f19582c6285a24368ff9be9e9fd9430
SyikhaSonOfAdin <syikhasmart@gmail.com>
2024-07-11 06:28:46 +0700

    Update Fixing Summary bug 1

--- src/modules/attendance/summary.js
+++ src/modules/attendance/summary.js
@@ -7,7 +7,7 @@ class Summary {
     getSummary = async (date) => {
         const CONNECTION = await SAT.getConnection();
         const QUERY = `
-            SELECT d.ID, d.NAME AS DEPARTMENTS, COALESCE(h.Presen, 0) AS PRESENT, COALESCE(a.Absent, 0) - COALESCE(n.Night_Shift, 0) AS ABSENT, t.Total AS TOTAL, COALESCE(n.Night_Shift, 0) AS NIGHT_SHIFT 
+            SELECT d.ID, d.NAME AS DEPARTMENTS, COALESCE(h.Presen, 0) AS PRESENT, COALESCE(a.Absent, 0) AS ABSENT, t.Total AS TOTAL, COALESCE(n.Night_Shift, 0) AS NIGHT_SHIFT 
             FROM (SELECT lw.DEPARTMENT_ID, COUNT(lw.ID) AS Total FROM ${LIST_WORKER.TABLE} lw GROUP BY lw.DEPARTMENT_ID) t 
             LEFT JOIN (SELECT lw.DEPARTMENT_ID, COUNT(wc.ID) AS Presen FROM ${LIST_WORKER.TABLE} lw 
             LEFT JOIN ${WORKER_CHECKIN.TABLE} wc ON lw.ID = wc.WORKER_ID WHERE wc.DATE = ? 
